6e6c41fe1e78e6b80f0bebba73dc2dc567803fb2
Jacob Chapman <33jake@gmail.com>
2019-04-29 19:18:11 -0700

    Added base sass files, react-flexbox, removed passport authentication.

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const showdown = require("showdown");
 const bodyParser = require("body-parser");
-const passport = require("passport");
-const jwt = require("jwt-simple");
 const path = require("path");
-const LocalStrategy = require("passport-local").Strategy;
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,47 +14,15 @@ app.use(express.static(path.join(__dirname, "build")));
 
 let converter = new showdown.Converter();
 
-const ADMIN = "username";
-const ADMIN_PASSWORD = "password";
-const SECRET = "secret";
-
-passport.use(
-  new LocalStrategy(function(username, password, done) {
-    if (username === ADMIN && password === ADMIN_PASSWORD) {
-      done(null, jwt.encode({ username }, SECRET));
-      return;
-    }
-    done(null, false);
-  })
-);
-
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
-
-app.post("/login", passport.authenticate("local", { session: false }), function(
-  req,
-  res
-) {
-  res.send("Authenticated");
-});
-
-app.post(
-  "/convert",
-  passport.authenticate("local", { session: false, failWithError: true }),
-  function(req, res, next) {
-    if (typeof req.body.content == "undefined" || req.body.content == null) {
-      res.json(["error", "No data found"]);
-    } else {
-      let text = req.body.content;
-      let html = converter.makeHtml(text);
-      res.json(["markdown", html]);
-    }
-  },
-  function(err, req, res, next) {
-    return res.status(401).send({ success: false, message: err });
+app.post("/convert", function(req, res) {
+  if (typeof req.body.content == "undefined" || req.body.content == null) {
+    res.json(["error", "No data found"]);
+  } else {
+    let text = req.body.content;
+    let html = converter.makeHtml(text);
+    res.json(["markdown", html]);
   }
-);
+});
 
 const port = process.env.REACT_APP_API_PORT || 8080;
 app.listen(port, function() {
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-const App = () => <div>Hello World!</div>;
+import { Grid, Row, Col } from "react-flexbox-grid";
+
+const App = () => (
+  <Grid>
+    <Row>
+      <Col xs={12} md={10} lg={8} mdOffset={1} lgOffset={2}>
+        <h1 class="text-center">{process.env.REACT_APP_NAME}</h1>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 export default App;
