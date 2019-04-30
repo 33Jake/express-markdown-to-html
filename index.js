fdbdf26385c443bc05bc0b68283410821831e83d
Jacob Chapman <33jake@gmail.com>
2019-04-29 18:21:16 -0700

    Added passport-local authentication.

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const showdown = require("showdown");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const jwt = require("jwt-simple");
+const LocalStrategy = require("passport-local").Strategy;
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var converter = new showdown.Converter();
+converter = new showdown.Converter();
+
+const ADMIN = "username";
+const ADMIN_PASSWORD = "password";
+const SECRET = "secret";
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    if (username === ADMIN && password === ADMIN_PASSWORD) {
+      done(null, jwt.encode({ username }, SECRET));
+      return;
+    }
+    done(null, false);
+  })
+);
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
-app.post("/login", function(req, res) {
+app.post("/login", passport.authenticate("local", { session: false }), function(
+  req,
+  res
+) {
   res.send("Authenticated");
 });
 
-app.post("/convert", function(req, res, next) {
-  if (typeof req.body.content == "undefined" || req.body.content == null) {
-    res.json(["error", "No data found"]);
-  } else {
-    text = req.body.content;
-    html = converter.makeHtml(text);
-    res.json(["markdown", html]);
+app.post(
+  "/convert",
+  passport.authenticate("local", { session: false, failWithError: true }),
+  function(req, res, next) {
+    if (typeof req.body.content == "undefined" || req.body.content == null) {
+      res.json(["error", "No data found"]);
+    } else {
+      text = req.body.content;
+      html = converter.makeHtml(text);
+      res.json(["markdown", html]);
+    }
+  },
+  function(err, req, res, next) {
+    return res.status(401).send({ success: false, message: err });
   }
-});
+);
 
 app.listen(3000, function() {
   console.log("Server running on port 3000");
